da33cb5a79363a92138bbc03ef2a30ca4594936d
developer.up90 <developer.up90@gmail.com>
2018-07-16 18:12:45 +0530

    vue 2 updated

--- vue2/src/main.spec.js
+++ vue2/src/main.spec.js
@@ -1,12 +1,13 @@
 import Vue from "vue";
 import App from "./main";
 
-describe("Adder", () => {
-  it("has a created hook", () => {
+describe("Vue component tests", () => {
+  it("should have function type", () => {
     expect(typeof App.add).toBe("function");
   });
+  
   it("should return 5", () => {
-    expect(typeof App.add(2,3)).toBe(5);
+    expect(App.add(2,3)).toBe(5);
   });
 
 });
