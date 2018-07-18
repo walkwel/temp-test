be90c5a488af879cbf0617da52f488ea6609a6de
developer.up90 <developer.up90@gmail.com>
2018-07-18 17:39:21 +0530

    react-jest repo modified

--- react-jest/index.spec.js
+++ react-jest/index.spec.js
@@ -4,15 +4,15 @@ import Adapter from 'enzyme-adapter-react-16';
 
 Enzyme.configure({adapter: new Adapter()});
 
-import Button from '../index';
+import Button from './index';
 
 it('Button text value after click', () => {
   // Render a checkbox with label in the document
-  const buttonComponent = shallow(<Button count={0} />);
+  const buttonComponent = shallow(<Button count={1} />);
 
-  expect(buttonComponent.text()).toEqual('0');
+  expect(buttonComponent.text()).toEqual('1');
 
   buttonComponent.find('button').simulate('click');
 
-  expect(buttonComponent.text()).toEqual('1');
+  expect(buttonComponent.text()).toEqual('2');
 });
