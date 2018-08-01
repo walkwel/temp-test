182d7d80f65275a9e326bdad2093189225bb784f
developer.up90 <developer.up90@gmail.com>
2018-08-01 16:20:22 +0530

    minor changes

--- es6/index.spec.js
+++ es6/index.spec.js
@@ -1,18 +1,18 @@
 let greetings = require("./index");
 
 describe("Calculation 1", () => {
-  it("knows that add(2,3) return 4", () => {
-    expect(greetings.add(2,3)).toBe(5);
+  it("knows that add(10,30) return 40", () => {
+    expect(greetings.add(10,30)).toBe(40);
   });
-  it("knows that subtract(2,3) return -1", () => {
-    expect(greetings.subtract(2,3)).toBe(-1);
+  it("knows that subtract(90,30) return 60", () => {
+    expect(greetings.subtract(90,30)).toBe(60);
   });
 });
 describe("Calculation 2", () => {
-  it("knows that multiple(2,3) return 4", () => {
-    expect(greetings.multiple(2,3)).toBe(6);
+  it("knows that multiple(20,30) return 600", () => {
+    expect(greetings.multiple(20,30)).toBe(600);
   });
-  it("knows that divide(6,3) return 2", () => {
-    expect(greetings.divide(6,3)).toBe(2);
+  it("knows that divide(600,30) return 20", () => {
+    expect(greetings.divide(600,30)).toBe(20);
   });
 });
